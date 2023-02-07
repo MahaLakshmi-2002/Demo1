7d150134190facbd449eeb23686e4e68b1fbf4cd
DIGSAN <DIGSAN@10.4.194.152>
2023-02-07 14:32:18 +0530

    commited

---
+++ MavenJavaProject/src/main/java/pkg_calculator/Calculator.java
@@ -0,0 +1,18 @@
+package pkg_calculator;
+
+public class Calculator {
+	int a,b;
+	public int addition(int a, int b) {
+		return a+b;
+	}
+	public int subtract(int a, int b) {
+		return a-b;
+	}
+	public int multiplication(int a, int b) {
+		return a*b;
+	}
+	public int division(int a, int b) {
+		return a/b;
+	}
+
+}
